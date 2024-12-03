20ffddb1e263907e3c574b545c5fef119d1b2164
LucaIgnatescu <lucaignatescu003@gmail.com>
2024-12-03 12:49:51 -0500

    added gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FlatEarthProject/',  // replace with your repository name
   plugins: [react()],
 })
